c14de08bed8e0e8049cb11c3fd8f72c3326a597d
Alfonso Buononato <74600557+AlfonsoBuononato@users.noreply.github.com>
2021-03-18 17:38:11 +0100

    esercizio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* 
+1.CHIEDERE ALL UTENTE IL SUO NOME
+2. POI CHIEDI IL SUO COGNOME,
+3.POI CHIEDI IL SUO COLORE PREFERITO
+4. SCRIVI SULLA PAGINA ;NOMECOGNOMECOLOREPREFERITO21
+*/
+
+var nome = prompt("come ti chiami?");
+
+var cognome = prompt("qual'è il tuo cognome?");
+
+var colorePreferito = prompt("qual'è il tuo colore preferito?");
+
+var password = document.getElementById("password").innerHTML = (nome + cognome + colorePreferito + 21)
